fc91e672d3181eb999c98d765b079a5f9be5fb07
xac <jiangfengxi.c@gmail.com>
2023-08-03 16:34:12 +0800

    camera update for 08

--- 08-lighting-map/src/camera.h
+++ 08-lighting-map/src/camera.h
@@ -1,34 +1,75 @@
 #pragma once
 #include <GLFW/glfw3.h>
+#include <cmath>
 #include <exception>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <stdexcept>
 
 namespace xac {
 class Camera {
-public:
+ public:
   enum class ProjectionMethod {
     ORTHO,
     PERSP,
+    UNKONWN,
   };
 
   Camera() = delete;
-  Camera(glm::vec3 position, glm::vec3 front, glm::vec3 up);
-  auto GetViewMatrix() -> glm::mat4;
-  auto GetProjectionMatrix() -> glm::mat4;
 
-  void UpdateScroll(double x_offset);
-  auto GetFornt() -> glm::vec3;
-  void SetFront(glm::vec3 front);
-  void PositionForward(glm::vec3 forward);
-  auto GetUp() -> glm::vec3;
-  auto GetPosition() -> glm::vec3;
+  Camera(glm::vec3 position, glm::vec3 target) : position_(position) {
+    front_ = glm::normalize(target - position);
+    pitch_ = std::asin(front_.y);
+    yaw_ = (front_.z < 0 ? -1 : 1) * glm::acos(front_.x / std::cos(pitch_));
+    UpdateVectors();
+  }
 
-private:
+  auto GetViewMatrix() -> glm::mat4 { return glm::lookAt(position_, position_ + front_, up_); }
+
+  auto GetProjectionMatrix() -> glm::mat4 {
+    if (projection_method_ == ProjectionMethod::PERSP) {
+      return glm::perspective(fov_, aspect_, near_, far_);
+    }
+    throw std::logic_error("not implemented");
+  }
+
+  void UpdateScroll(double y_offset) {
+    if (fov_ >= 1.0f && fov_ <= 45.0f) {
+      fov_ -= static_cast<float>(y_offset) * 0.05;
+    }
+    if (fov_ <= 1.0f) {
+      fov_ = 1.0f;
+    }
+    if (fov_ >= 45.0f) {
+      fov_ = 45.0f;
+    }
+  }
+
+  void UpdateCursorMove(float x_offset, float y_offset) {
+    yaw_ += x_offset * Camera::sensitivity_;
+    pitch_ -= y_offset * Camera::sensitivity_;
+    CheckPitchSafety();
+    UpdateVectors();
+  }
+
+  auto GetFornt() -> glm::vec3 { return front_; }
+
+  void PositionForward(glm::vec3 forward) { position_ += forward; }
+
+  auto GetUp() -> glm::vec3 { return up_; }
+
+  auto GetPosition() -> glm::vec3 { return position_; }
+
+ private:
   glm::vec3 position_;
   glm::vec3 front_;
   glm::vec3 up_;
+  float yaw_;
+  float pitch_;
+
+  static glm::vec3 world_up_;
+  static float sensitivity_;
 
   ProjectionMethod projection_method_ = ProjectionMethod::PERSP;
 
@@ -36,41 +77,26 @@ private:
   float aspect_ = 1.0f;
   float near_ = 0.1f;
   float far_ = 100.0f;
+
+  void CheckPitchSafety() {
+    if (pitch_ > 1.5533f) {
+      pitch_ = 1.5533f;
+    } else if (pitch_ < -1.5533f) {
+      pitch_ = -1.5533f;
+    }
+  }
+
+  void UpdateVectors() {
+    front_.y = std::sin(pitch_);
+    front_.x = std::cos(pitch_) * std::cos(yaw_);
+    front_.z = std::cos(pitch_) * std::sin(yaw_);
+    front_ = glm::normalize(front_);
+    glm::vec3 right = glm::normalize(glm::cross(front_, Camera::world_up_));
+    up_ = glm::normalize(glm::cross(right, front_));
+  };
 };
 
-Camera::Camera(glm::vec3 position, glm::vec3 front, glm::vec3 up)
-    : position_(position), front_(front), up_(up) {}
+glm::vec3 Camera::world_up_{0, 1, 0};
+float Camera::sensitivity_ = 0.002;
 
-auto Camera::GetViewMatrix() -> glm::mat4 {
-  return glm::lookAt(position_, position_ + front_, up_);
-}
-auto Camera::GetProjectionMatrix() -> glm::mat4 {
-  if (projection_method_ == ProjectionMethod::PERSP) {
-    return glm::perspective(fov_, aspect_, near_, far_);
-  }
-  throw std::logic_error("not implemented");
-}
-
-void Camera::UpdateScroll(double y_offset) {
-  if (fov_ >= 1.0f && fov_ <= 45.0f) {
-    fov_ -= static_cast<float>(y_offset) * 0.05;
-  }
-  if (fov_ <= 1.0f) {
-    fov_ = 1.0f;
-  }
-  if (fov_ >= 45.0f) {
-    fov_ = 45.0f;
-  }
-}
-
-auto Camera::GetFornt() -> glm::vec3 { return front_; }
-
-void Camera::SetFront(glm::vec3 front) { front_ = front; }
-
-auto Camera::GetUp() -> glm::vec3 { return up_; }
-
-auto Camera::GetPosition() -> glm::vec3 { return position_; }
-
-void Camera::PositionForward(glm::vec3 forward) { position_ += forward; }
-
-} // end namespace xac
+}  // end namespace xac
