907c0abdb6b4aaff1a33c797a1d6125aefbddfe0
xac <jiangfengxi.c@gmail.com>
2023-10-31 19:44:06 +0800

    [format, refactor](nanoR) use enum in namespace instead of static

--- nanoR/src/event/event.h
+++ nanoR/src/event/event.h
@@ -10,31 +10,50 @@ enum class EventType {
   kKeyDown, kKeyUp, 
   kMouseCursorMove, kMouseButtonDown, kMouseButtonUp, kMouseButtonScroll,
 };
-// clang-format on 
 
+// clang-format on
 
-// HINT: enum class cannot use operator | 
-struct EventCategory {
-  static const uint32_t kUnknown  = 1;
-  static const uint32_t kInput    = 1 << 1;
-  static const uint32_t kKey      = 1 << 2;
-  static const uint32_t kMouse    = 1 << 3;
-  static const uint32_t kWindow   = 1 << 4;
+namespace EventCategory {
 
-  static const uint32_t kMaxSize  = 1 << 30;
+// HINT: enum class cannot use operator |
+enum type {
+  kUnknown = 1,
+  kInput = 1 << 1,
+  kKey = 1 << 2,
+  kMouse = 1 << 3,
+  kWindow = 1 << 4,
+
+  kMaxSize = 1 << 30,
 };
+}  // namespace EventCategory
 
-#define DECLARE_EVENT_TYPE(event_type) static auto Type() -> EventType { return EventType::event_type; } \
-                                       auto GetType() const -> EventType override { return EventType::event_type; } \
-                                       auto GetName() const -> std::string override { return "Event::" #event_type; }
+#define DECLARE_EVENT_TYPE(event_type)         \
+  static auto Type()->EventType {              \
+    return EventType::event_type;              \
+  }                                            \
+  auto GetType() const->EventType override {   \
+    return EventType::event_type;              \
+  }                                            \
+  auto GetName() const->std::string override { \
+    return "Event::" #event_type;              \
+  }
 
-#define DECLARE_EVENT_CATEGORY(event_category) auto GetCategory() const -> uint32_t override { return event_category; }
+#define DECLARE_EVENT_CATEGORY(event_category)  \
+  auto GetCategory() const->uint32_t override { \
+    return event_category;                      \
+  }
 
 class Event {
-public:
-  virtual auto ToString() const -> std::string { return fmt::format("[{}] ToString NOT IMPLEMENTED", GetName()); } 
-  virtual auto GetType() const -> EventType { return EventType::kUnknown; } 
-  virtual auto GetName() const -> std::string { return "EventType::kUnknown"; } 
+ public:
+  virtual auto ToString() const -> std::string {
+    return fmt::format("[{}] ToString NOT IMPLEMENTED", GetName());
+  }
+  virtual auto GetType() const -> EventType {
+    return EventType::kUnknown;
+  }
+  virtual auto GetName() const -> std::string {
+    return "EventType::kUnknown";
+  }
   virtual auto GetCategory() const -> uint32_t = 0;
 };
 
